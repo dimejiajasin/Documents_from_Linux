ed8afeed541e06c975fbb898eb70f123fddd05e5
Dimeji Ajasin <daaajasin@gmail.com>
2022-11-26 10:43:28 -0800

    new files from linux

---
+++ app.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+""" 
+Comments here
+"""
+import os
+path=os.getcwd()
+print(path)
+print("Hello Dimeji")
---
+++ script01.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+<<c
+Place Commments here
+c
+
+echo `date`
+echo Dimeji Ajasin
